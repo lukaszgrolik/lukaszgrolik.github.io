7b7cff03aabb990074e87f9adfcd86773e7d8587
Łukasz Grolik <lukasz@grolik.pl>
2024-11-12 04:03:14 +0100

    content update - webdev, gamedev

---
+++ d5b40635d415f5c996d15dea6932c9091ed7e1bd-137b85bd01b3673474e4.js
@@ -0,0 +1,123 @@
+"use strict";(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[174],{9777:function(e,t,a){a.d(t,{A:function(){return u}});var n=a(6540),l=a(6143),r=a(4578);const c=l.A.div`
+    background-color: #eee;
+    margin-top: 1em;
+    line-height: 1;
+
+    iframe {
+        display: block;
+        margin: 0 auto;
+    }
+`,s=l.A.ul`
+    > * + * {
+        margin-top: 2em;
+    }
+`,m=l.A.div`
+    > * + * {
+        margin-top: 1em;
+    }
+`,i=l.A.div`
+    background-color: #eee;
+
+    video, img {
+        max-width: 640px;
+        margin: 0 auto;
+        display: block;
+    }
+`,o=l.A.div`
+    background-color: #eee;
+    display: flex;
+    justify-content: center;
+
+    :first-of-type {
+        /* max-width: 640px; */
+        /* margin: 0 auto;
+        display: block; */
+    }
+`,g=l.A.h3`
+    color: rgba(0, 0, 0, .5);
+    font-size: .75em;
+    line-height: 1;
+    padding-bottom: .25em;
+    border-bottom: 1px solid rgba(0, 0, 0, .1);
+`;function h(e){const{imageGroup:t}=e;return n.createElement(m,null,n.createElement(g,null,t.title),t.Content&&n.createElement(r.VJ,null,n.createElement(t.Content,null)),t.gfycatId?n.createElement(i,null,n.createElement("video",{autoPlay:!1,playsInline:!1,preload:"auto",poster:`https://thumbs.gfycat.com/${t.gfycatId}-mobile.jpg`,tabIndex:-1,controls:!0},n.createElement("source",{src:`https://thumbs.gfycat.com/${t.gfycatId}-mobile.mp4`,type:"video/mp4"}),n.createElement("source",{src:`https://giant.gfycat.com/${t.gfycatId}.webm`,type:"video/webm"}),n.createElement("source",{src:`https://giant.gfycat.com/${t.gfycatId}.mp4`,type:"video/mp4"}),n.createElement("source",{src:`https://thumbs.gfycat.com/${t.gfycatId}-mobile.mp4`,type:"video/mp4"}))):t.gif&&n.createElement(i,null,n.createElement("img",{src:t.gif,alt:t.gif})),n.createElement(r.bs,null,t.paths.map(((e,a)=>n.createElement("div",{key:e},n.createElement("img",{loading:"lazy",src:`${t.basePath}${e}`,width:300,height:300}))))),t.Tweet&&n.createElement(o,null,n.createElement(t.Tweet,null)))}const p=[{title:"Madfractal",basePath:"/images/madfractal/",paths:["vlcsnap-2021-01-20-23h13m18s110_square.png","vlcsnap-2021-01-20-23h14m49s967_square.png","vlcsnap-2021-01-24-23h22m30s825_square.png","screenshot_3840x2160_2020-07-19_12-33-08_square.png","screenshot_3840x2160_2020-07-19_12-49-45_square.png","screenshot_3840x2160_2020-07-19_12-46-12_square.png","screenshot_3840x2160_2020-07-19_12-42-38_square.png","church 2_square.png","devlog-6-thumbnail-bg_square.png","devlog-5-thumbnail-bg_square.png","devlog-2-thumbnail-bg_square.png"],gif:"https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExaHM2ZTl2cjJwdmRnaTdndDkxOWZqY3cxNHRzY283NjVweGp1MXl6NiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/QkicdZnx66QhJWm0Ba/giphy.gif",Content:()=>n.createElement(n.Fragment,null,n.createElement("p",null,"My main side-project is a computer game called ",n.createElement("strong",null,"Madfractal")," which I've been working on since 2019 using the ",n.createElement("strong",null,"Unity")," engine. It's heavily inspired by the ",n.createElement("strong",null,"Diablo II")," game which I used to play a lot. I think the dark, gothic atmosphere of the game is what made me take a chance at implementing my own take on the action-RPG genre."),n.createElement("p",null,"You can visit ",n.createElement("a",{href:"https://madfractal.com",target:"__blank"},"madfractal.com")," and my gamedev Twitter channel ",n.createElement("a",{href:"https://twitter.com/LukGameDev",target:"__blank"},"@LukGameDev")," for latest news."))},{title:"Untitled economy RTS game",basePath:"/images/economy-rts/",paths:["screenshot_2021-03-24_13-47-41_1920x1080_square.png","screenshot_2021-03-17_21-53-19_1920x1080_square.png","screenshot_2021-03-17_17-03-20_1920x1080_square.png"],gif:"https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExcnB3Z3MyY2txcG1kYXNlbzczMWR5d3Jkb2xob3R2MXQwbG92dzh6dyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/XKXoN2KvnfzrGvzvv5/giphy.gif",Content:()=>n.createElement(n.Fragment,null,n.createElement("p",null,"I've also been working on an economy strategy game inspired by the late 1990s and early 2000s Sierra games like ",n.createElement("strong",null,"Caesar"),", ",n.createElement("strong",null,"Pharaoh")," and ",n.createElement("strong",null,"Zeus"),", using hand-drawn graphics style."))},{title:"Untitled platformer game",basePath:"/images/platformer/",paths:["vlcsnap-2024-10-28-01h22m34s161_square.png","vlcsnap-2024-10-28-01h23m27s091_square.png","vlcsnap-2024-10-28-01h23m50s083_square.png","vlcsnap-2024-10-28-01h24m12s780_square.png","vlcsnap-2024-10-28-01h24m28s188_square.png","vlcsnap-2024-10-28-01h25m05s944_square.png","vlcsnap-2024-10-28-01h25m24s068_square.png","vlcsnap-2024-10-28-01h26m07s624_square.png","vlcsnap-2024-10-28-01h26m42s998_square.png"],gif:"https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExYjJxMjc5MXpnc2psbXF1eWVjMnA0bGllY29sM2VzazU3NWQ3MGJ2YiZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/90hktKxiqTwhyQ1noN/giphy.gif"},{title:"Untitled 2d hack'n'slash game",basePath:"/images/hackan/",paths:["Screenshot (29)_square.jpg","Screenshot (31)_square.jpg","Screenshot (34)_square.jpg","Screenshot (35)_square.jpg","Screenshot (40)_square.jpg","Screenshot (41)_square.jpg","Screenshot (45)_square.jpg","Screenshot (48)_square.jpg"]},{title:"Unity side-projects & experiments",basePath:"/images/projects/",paths:["city-builder_square.png","forest at the beach 1 screen_2560x1440_2019-06-06_21-45-38_square.png","screenshot-2019-04-18-103128813_square.png","screenshot-2019-04-18-103424481_square.png"],gif:"https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExM213bWw3MThzYTMycHAxbDg2MGZlZGQ3eDl5NnQ0d215OHQxNHkzaSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/v83OHuXu4RPDasV9z1/giphy.gif"}];function u(e){const{content:t}=e;return n.createElement("div",null,n.createElement("div",{style:{}},n.createElement(r.VJ,null,n.createElement("h2",null,"Gamedev"),n.createElement("p",null,"I have a ",n.createElement("a",{href:"https://www.youtube.com/channel/UCD4zBy-bxuTdt3UUMowjcaA"},"YouTube channel")," where I mainly post devlogs among other gamedev-related experiments.")),n.createElement(c,null,n.createElement("iframe",{width:"560",height:"315",src:"https://www.youtube-nocookie.com/embed/wPkDTSG733Y",frameBorder:"0",allow:"accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture",allowFullScreen:!0})),t&&n.createElement(r.VJ,{style:{marginTop:"1em"}},n.createElement(t,null)),e.extended&&n.createElement(n.Fragment,null,n.createElement("div",{style:{marginTop:"2em"}},n.createElement(s,null,p.map((e=>n.createElement("li",{key:e.title},n.createElement(h,{imageGroup:e})))))))))}},4273:function(e,t,a){a.d(t,{A:function(){return y}});var n=a(6540),l=a(6143);function r(e){return n.createElement("svg",{role:"img",viewBox:"0 0 24 24",xmlns:"http://www.w3.org/2000/svg",style:{height:"2em"},fill:e.fill},n.createElement("path",{d:"M20.222 0c1.406 0 2.54 1.137 2.607 2.475V24l-2.677-2.273-1.47-1.338-1.604-1.398.67 2.205H3.71c-1.402 0-2.54-1.065-2.54-2.476V2.48C1.17 1.142 2.31.003 3.715.003h16.5L20.222 0zm-6.118 5.683h-.03l-.202.2c2.073.6 3.076 1.537 3.076 1.537-1.336-.668-2.54-1.002-3.744-1.137-.87-.135-1.74-.064-2.475 0h-.2c-.47 0-1.47.2-2.81.735-.467.203-.735.336-.735.336s1.002-1.002 3.21-1.537l-.135-.135s-1.672-.064-3.477 1.27c0 0-1.805 3.144-1.805 7.02 0 0 1 1.74 3.743 1.806 0 0 .4-.533.805-1.002-1.54-.468-2.14-1.404-2.14-1.404s.134.066.335.2h.06c.03 0 .044.015.06.03v.006c.016.016.03.03.06.03.33.136.66.27.93.4.466.202 1.065.403 1.8.536.93.135 1.996.2 3.21 0 .6-.135 1.2-.267 1.8-.535.39-.2.87-.4 1.397-.737 0 0-.6.936-2.205 1.404.33.466.795 1 .795 1 2.744-.06 3.81-1.8 3.87-1.726 0-3.87-1.815-7.02-1.815-7.02-1.635-1.214-3.165-1.26-3.435-1.26l.056-.02zm.168 4.413c.703 0 1.27.6 1.27 1.335 0 .74-.57 1.34-1.27 1.34-.7 0-1.27-.6-1.27-1.334.002-.74.573-1.338 1.27-1.338zm-4.543 0c.7 0 1.266.6 1.266 1.335 0 .74-.57 1.34-1.27 1.34-.7 0-1.27-.6-1.27-1.334 0-.74.57-1.338 1.27-1.338z"}))}function c(e){return n.createElement("svg",{role:"img",viewBox:"0 0 24 24",xmlns:"http://www.w3.org/2000/svg",style:{height:"2em"},fill:e.fill},n.createElement("path",{d:"M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12"}))}function s(e){return n.createElement("svg",{role:"img",viewBox:"0 0 24 24",xmlns:"http://www.w3.org/2000/svg",style:{height:"2em"},fill:e.fill},n.createElement("path",{d:"M12 0C8.74 0 8.333.015 7.053.072 5.775.132 4.905.333 4.14.63c-.789.306-1.459.717-2.126 1.384S.935 3.35.63 4.14C.333 4.905.131 5.775.072 7.053.012 8.333 0 8.74 0 12s.015 3.667.072 4.947c.06 1.277.261 2.148.558 2.913.306.788.717 1.459 1.384 2.126.667.666 1.336 1.079 2.126 1.384.766.296 1.636.499 2.913.558C8.333 23.988 8.74 24 12 24s3.667-.015 4.947-.072c1.277-.06 2.148-.262 2.913-.558.788-.306 1.459-.718 2.126-1.384.666-.667 1.079-1.335 1.384-2.126.296-.765.499-1.636.558-2.913.06-1.28.072-1.687.072-4.947s-.015-3.667-.072-4.947c-.06-1.277-.262-2.149-.558-2.913-.306-.789-.718-1.459-1.384-2.126C21.319 1.347 20.651.935 19.86.63c-.765-.297-1.636-.499-2.913-.558C15.667.012 15.26 0 12 0zm0 2.16c3.203 0 3.585.016 4.85.071 1.17.055 1.805.249 2.227.415.562.217.96.477 1.382.896.419.42.679.819.896 1.381.164.422.36 1.057.413 2.227.057 1.266.07 1.646.07 4.85s-.015 3.585-.074 4.85c-.061 1.17-.256 1.805-.421 2.227-.224.562-.479.96-.899 1.382-.419.419-.824.679-1.38.896-.42.164-1.065.36-2.235.413-1.274.057-1.649.07-4.859.07-3.211 0-3.586-.015-4.859-.074-1.171-.061-1.816-.256-2.236-.421-.569-.224-.96-.479-1.379-.899-.421-.419-.69-.824-.9-1.38-.165-.42-.359-1.065-.42-2.235-.045-1.26-.061-1.649-.061-4.844 0-3.196.016-3.586.061-4.861.061-1.17.255-1.814.42-2.234.21-.57.479-.96.9-1.381.419-.419.81-.689 1.379-.898.42-.166 1.051-.361 2.221-.421 1.275-.045 1.65-.06 4.859-.06l.045.03zm0 3.678c-3.405 0-6.162 2.76-6.162 6.162 0 3.405 2.76 6.162 6.162 6.162 3.405 0 6.162-2.76 6.162-6.162 0-3.405-2.76-6.162-6.162-6.162zM12 16c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4zm7.846-10.405c0 .795-.646 1.44-1.44 1.44-.795 0-1.44-.646-1.44-1.44 0-.794.646-1.439 1.44-1.439.793-.001 1.44.645 1.44 1.439z"}))}function m(e){return n.createElement("svg",{role:"img",xmlns:"http://www.w3.org/2000/svg",viewBox:"0 0 24 24",style:{height:"2em"},fill:e.fill},n.createElement("path",{d:"M23.953 4.57a10 10 0 01-2.825.775 4.958 4.958 0 002.163-2.723c-.951.555-2.005.959-3.127 1.184a4.92 4.92 0 00-8.384 4.482C7.69 8.095 4.067 6.13 1.64 3.162a4.822 4.822 0 00-.666 2.475c0 1.71.87 3.213 2.188 4.096a4.904 4.904 0 01-2.228-.616v.06a4.923 4.923 0 003.946 4.827 4.996 4.996 0 01-2.212.085 4.936 4.936 0 004.604 3.417 9.867 9.867 0 01-6.102 2.105c-.39 0-.779-.023-1.17-.067a13.995 13.995 0 007.557 2.209c9.053 0 13.998-7.496 13.998-13.985 0-.21 0-.42-.015-.63A9.935 9.935 0 0024 4.59z"}))}function i(e){return n.createElement("svg",{role:"img",xmlns:"http://www.w3.org/2000/svg",viewBox:"0 0 24 24",style:{height:"2em"},fill:e.fill},n.createElement("path",{d:"M23.498 6.186a3.016 3.016 0 0 0-2.122-2.136C19.505 3.545 12 3.545 12 3.545s-7.505 0-9.377.505A3.017 3.017 0 0 0 .502 6.186C0 8.07 0 12 0 12s0 3.93.502 5.814a3.016 3.016 0 0 0 2.122 2.136c1.871.505 9.376.505 9.376.505s7.505 0 9.377-.505a3.015 3.015 0 0 0 2.122-2.136C24 15.93 24 12 24 12s0-3.93-.502-5.814zM9.545 15.568V8.432L15.818 12l-6.273 3.568z"}))}const o="1280px",g=l.A.div`
+    display: flex;
+    /* justify-content: center; */
+    align-items: center;
+
+    @media screen and (min-width: ${o}) {
+        > * + * {
+            margin-left: 2em;
+        }
+    }
+
+    img {
+        border-radius: 100%;
+        display: block;
+        max-width: 200px;
+        max-height: 200px;
+    }
+`,h=l.A.div`
+    display: none;
+
+    @media screen and (min-width: ${o}) {
+        display: block;
+    }
+`,p=l.A.div`
+    > * + * {
+        margin-top: 1em;
+    }
+`,u=l.A.div`
+    color: #aaa;
+    line-height: 1.5;
+`,d=l.A.ul`
+    display: flex;
+    flex-wrap: wrap;
+    line-height: 1;
+
+    li {
+        display: flex;
+        align-items: stretch;
+    }
+
+    > * + * {
+        /* margin-left: 1em; */
+    }
+
+`,f=l.A.a`
+    display: block;
+    line-height: 1;
+    text-align: center;
+    vertical-align: middle;
+    max-width: 3em;
+
+    > * + * {
+        margin-top: .5em;
+    }
+
+    &:not(:hover) .social-icon-image {
+        background-color: #fff !important;
+    }
+
+    &:hover .social-icon-image {
+        svg {
+            fill: #fff;
+        }
+    }
+`,w=l.A.div`
+    padding: .5em .5em;
+    transition: background-color .2s;
+
+    svg {
+        /* fill: rgba(0, 0, 0, .15); */
+        display: block;
+        transition: fill .2s;
+    }
+`,v=l.A.div`
+    color: rgba(0, 0, 0, .5);
+    font-size: .4em;
+    line-height: 1.5;
+    display: block;
+    text-align: center;
+    text-transform: uppercase;
+`;function y(){const e=[{name:"Gamedev Channel",Icon:i,color:"#FF0000",url:"https://www.youtube.com/channel/UCD4zBy-bxuTdt3UUMowjcaA"},{name:"Gamedev Twitter",Icon:m,color:"#1DA1F2",url:"https://twitter.com/LukGameDev"},{name:"Discord",Icon:r,color:"#7289DA",url:"https://discord.gg/s536z5Y"},{name:"Photos",Icon:s,color:"#E4405F",url:"https://www.instagram.com/_lukg_/"},{name:"Graphic Art",Icon:s,color:"#E4405F",url:"https://www.instagram.com/lukg_creations/"},{name:"GitHub",Icon:c,color:"#181717",url:"https://github.com/lukaszgrolik"}];return n.createElement(g,null,n.createElement(h,null,n.createElement("img",{src:"/images/hns game assets 2 v9 256x256.png"})),n.createElement(p,null,n.createElement(u,null,n.createElement("p",null,"Hi, I'm Łukasz. ",n.createElement("span",null,"You can call me Lucas or Luke.")),n.createElement("p",null,"I live in Poland."),n.createElement("p",null,"I do programming, music and graphic art.")),n.createElement("div",null,n.createElement(d,null,e.map((e=>n.createElement("li",{key:e.name},n.createElement(f,{href:e.url,target:"_blank"},n.createElement(w,{className:"social-icon-image",style:{backgroundColor:e.color}},n.createElement(e.Icon,{fill:e.color})),n.createElement(v,null,e.name)))))))))}}}]);
+//# sourceMappingURL=d5b40635d415f5c996d15dea6932c9091ed7e1bd-137b85bd01b3673474e4.js.map
